b9e2cb220c145a41640c7a422956e1dea7243ffb
Ayomal Kaushalya Banneka <166504527+ayomalbanneka@users.noreply.github.com>
2024-04-09 16:15:03 +0530

    Add files via upload

---
+++ Web assignment 1/script.js
@@ -0,0 +1,32 @@
+function send(){
+    alert("Email send Successfully. You will recieve the verification code shortly.");
+}
+
+function contact(){
+    alert("Thank you for contact us.");
+}
+
+function signin(){
+    alert("Sign In Success");
+}
+
+function signup(){
+    alert("User registration complete.");
+}
+
+function adminsignin(){
+    alert("Admin Sign In Success");
+}
+
+function cart(){
+    window.location = "cart.html";
+}
+
+function watchlist(){
+    window.location = "watchlist.html";
+}
+
+function sell(){
+    window.location = "sell.html";
+}
+
